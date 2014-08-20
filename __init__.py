c66efdef1e7d2164bd23a0e3a51f573b7755ea45
Ran <liran319@gmail.com>
2014-08-20 16:41:18 +0800

    fix bugs

--- __init__.py
+++ __init__.py
@@ -17,26 +17,6 @@ from urlsTools import *
    copy2txt:
    更新一个新的文本文件, 包含所有可用url重复多次后的结果
 """
-# urlsPath = r"C:\Users\li_ran\Desktop\url"
+
 
 __all__ = "__URLFilter", "copy_urls", "verify2txt", "copy2txt"
-# urlsPath = sys.argv[1]
-
-
-# def main():
-#     file_list = os.listdir(urlsPath)
-#     filespath = [os.path.join(urlsPath, i).replace("\\", "/") for i in file_list]
-#     for each_file in filespath:
-#         if each_file.find("_filted.txt") >= 0 or each_file.find("_copy.txt") >= 0:
-#             pass
-#         else:
-#             print "正在处理: " + each_file
-#             handler = UrlsHandler(each_file)
-#             handler.verify2txt()
-#             # handler.copy2txt()
-#             print "该文件处理完毕！"
-#     print "太棒啦！所有文件都处理好啦:)"
-
-
-# if __name__ == '__main__':
-#     main()
